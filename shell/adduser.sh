491c31ce24ba63826c6de8f0fbb61aee7a9edd75
root <root@sjs_75_38.(none)>
2017-07-11 12:01:48 +0800

    add shel

---
+++ shell/adduser.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+creat_user(){
+useradd $user
+echo "$pass" | passwd --stdin $user
+}
+
+let seed=($RANDOM%2+1)
+
+users='lyl ffy zhoumi wch chr loki cyl llm dlj'
+
+for user in $users
+do
+	pass=$user"123"
+	homedir="/search/data$seed/$user"
+	useradd $user -d $homedir -m -g users
+
+	echo "$pass" | passwd --stdin $user
+
+	let seed=seed%2+1
+done
+
